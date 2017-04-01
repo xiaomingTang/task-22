4f7ab35b67113848a626c77f61980bd745c5c5e6
qiulinTang <1038761793@qq.com>
2017-04-01 17:23:45 +0800

    Update js-tree.js

--- js-tree.js
+++ js-tree.js
@@ -1,1 +1,30 @@
-document.write("你好");
+//完全二叉树
+function FullBinTree(n){
+  this.gene=n;
+  this.frag=document.createDocumentFragment();
+  function init(){
+    buildTree(this.frag,this.gene);
+    return this.frag;
+  }();
+}
+FullBinTree.prototype={
+  constructor:FullBinTree,
+  
+  buildTree:function(elem,n){
+    var left=this.newElem(),
+        right=this.newElem();
+    elem.appendChild(left);
+    elem.appendChild(right);
+    if(n--){
+      this.buildTree(left,n);
+      this.buildTree(right,n);
+    }
+  },
+      
+
+  newElem:function(){
+    return document.createElement("div");
+  }
+}
+
+document.body.appendChild(new FullBinTree(4));
